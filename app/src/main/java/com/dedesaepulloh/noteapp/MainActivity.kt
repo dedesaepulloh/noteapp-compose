08b78ac24afc5141c8c73cbf919357a436d3324d
dedesaepulloh <dedesaepulloh77@gmail.com>
2021-10-11 12:13:27 +0700

    setup theme

--- app/src/main/java/com/dedesaepulloh/noteapp/MainActivity.kt
+++ app/src/main/java/com/dedesaepulloh/noteapp/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.dedesaepulloh.noteapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.dedesaepulloh.noteapp.ui.theme.NoteAppTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent { 
+            NoteAppTheme {
+                
+            }
+        }
     }
 }
